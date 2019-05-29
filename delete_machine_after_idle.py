84c4e72de84ea2ee6a8c63c6edc922273a7e7f11
bw2 <weisburd@broadinstitute.org>
2019-05-29 14:13:58 -0400

    finished implementing

--- delete_machine_after_idle.py
+++ delete_machine_after_idle.py
@@ -8,14 +8,14 @@ import time
 logging.basicConfig(level="INFO", format='%(asctime)-15s %(levelname)s: %(message)s')
 
 p = argparse.ArgumentParser(
-    description="Run this script on a machine in the background to shutdown the machine when load stays below some threshold for some number of mintues", 
+    description="Run this script on a machine in the background to delete the machine when load stays below some threshold for some number of mintues", 
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 p.add_argument("-L", "--load-threshold", type=float, default=0.2, help="utpime load below this threshold will be considered idle")
-p.add_argument("--minutes", type=float, default=60, help="shutdown after idle for this many minutes")
+p.add_argument("--minutes", type=float, default=60, help="delete machine after idle for this many minutes")
 args = p.parse_args()
 
 idle_minutes_counter = 0
-logging.info("This machine will shutdown if load stays <= {} for {} minutes...".format(args.load_threshold, args.minutes))
+logging.info("This machine will be deleted if load stays <= {} for {} minutes...".format(args.load_threshold, args.minutes))
 
 SLEEP_SECONDS = 6
 while idle_minutes_counter < args.minutes:
@@ -30,12 +30,12 @@ while idle_minutes_counter < args.minutes:
 
     if current_load <= args.load_threshold:
         idle_minutes_counter += SLEEP_SECONDS/60.0
-        logging.info("Current load = {}. This machine will shutdown if load stays <= {} for {:0.1f} more minutes.".format(
+        logging.info("Current load = {}. This machine will be deleted if load stays <= {} for {:0.1f} more minutes.".format(
                 current_load, args.load_threshold, args.minutes - idle_minutes_counter))
     else:
         logging.info("Current load = {}. Idle time reset to 0.".format(current_load))
         idle_minutes_counter = 0
 
-logging.info("Shutting down...")
-command = ["sudo", "powerdown"]
+logging.info("Deleting machine...")
+command = ["sudo", "delete this machine"]
 subprocess.check_output(command)
